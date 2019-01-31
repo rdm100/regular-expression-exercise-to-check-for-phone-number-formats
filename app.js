a4227f5b2e608afded1257f44ca225538919e28a
rdm100 <robertode_marco@hotmail.com>
2019-01-31 14:14:43 +0000

    Regular expression phone number format checking

---
+++ app.js
@@ -0,0 +1,33 @@
+/*
+Validate phone numbers entered into the text field. As the number is entered, check to see if it matches these formats: (nnn)-nnn-nnnn, nnn.nnn.nnnn, nnn-nnn-nnnn, nnnnnnnnnn, (nnn)nnn-nnnn. If the number matches, change the text color from red to green.
+
+Use several different phone numbers to test.
+
+HINT: You can use the keyup event to respond to entered text. There is a CSS Class for red and green.
+*/
+
+
+let phone = document.querySelector('#phone');
+let regPhone = /\(?\d{3}\)?[-.]?\d{3}[-.]?\d{4}$/;
+
+// phone.addEventListener("keyup", function() {
+//     if (phone.value.match(regPhone)) {
+//         phone.classList.remove("red");
+//         phone.classList.add("green");
+//     } else {
+//         phone.classList.remove("green");
+//         phone.classList.add("red");
+//     }
+// });
+
+phone.addEventListener("keyup", colourChange);
+
+function colourChange() {
+    if (phone.value.match(regPhone)) {
+        phone.classList.remove("red");
+        phone.classList.add("green");
+    } else {
+        phone.classList.remove("green");
+        phone.classList.add("red");
+    }
+}
